072694f585f414922d71a06fcbe918a1ba1c0d0f
Aquasterr <110834755+Aquasterr@users.noreply.github.com>
2022-09-26 11:35:39 -0300

    Update ex4.cs

--- ex4.cs
+++ ex4.cs
@@ -4,19 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Program
+namespace ConsoleApp1
 {
-    /*a) média da idade do grupo;
-    b) média da altura das mulheres;
-    c) média da idade dos homens;
-    d) percentual de pessoas com idade entre 18 e 35 anos (inclusive).
-     */
-    class Exercicio1
+    class Program
     {
+        /*a) média da idade do grupo;
+        b) média da altura das mulheres;
+        c) média da idade dos homens;
+        d) percentual de pessoas com idade entre 18 e 35 anos (inclusive).
+        */
         static void Main(string[] args)
         {
-            int idadeF, idadeM;
-            for (int i = 0; i < 1000; i++)
+            int homem = 0, mulher = 0, idadeF = 0, idadeM = 0, idadeP = 0;
+            double alturaF = 0, alturaM = 0;
+            for (int i = 0; i <= 4; i++)
             {
                 Console.WriteLine(" Informe o Sexo: \n");
                 Console.WriteLine(" 0 - Feminino ");
@@ -26,15 +27,45 @@ namespace Program
 
                 if (opc == 0)
                 {
+                    mulher++;
                     Console.WriteLine(" Informe a Idade: \n");
                     int idade = int.Parse(Console.ReadLine());
+                    if (idade > 17 && idade < 36) { idadeP++; }
+                    idadeF += idade;
+
+                    Console.WriteLine(" Informe a Altura: \n");
+                    double altura = double.Parse(Console.ReadLine());
+                    alturaF += altura;
+
+
                 }
                 if (opc == 1)
                 {
+                    homem++;
+                    Console.WriteLine(" Informe a Idade: \n");
+                    int idade = int.Parse(Console.ReadLine());
+                    if (idade > 17 && idade < 36) { idadeP++; }
+                    idadeM += idade;
 
+                    Console.WriteLine(" Informe a Altura: \n");
+                    double altura = double.Parse(Console.ReadLine());
+                    alturaM += altura;
                 }
             }
+            double mediaIdadeG, mediaAlturaF, mediaIdadeM;
 
+            mediaIdadeG = idadeF + idadeM / 4;
+            Console.WriteLine("Média da idade do grupo: " + mediaIdadeG);
+
+            mediaAlturaF = alturaF / mulher;
+            Console.WriteLine("Média de altura das mulheres: " + mediaAlturaF);
+
+            mediaIdadeM = idadeM / homem;
+            Console.WriteLine("Média da idade dos homens: " + mediaAlturaF);
+
+            double percentual = (idadeP / 4) / 100;
+            Console.WriteLine(idadeP);
+            Console.WriteLine("Percentual de pessoas com idade entre 18 e 35 anos: " + percentual);
         }
     }
 }
